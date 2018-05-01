c5d27b8a1dbb97e1d8ae5ca299f705766e19cd79
NINE\Ben-m <ben@xara.com>
2018-05-02 00:15:37 +0100

    This is the start of the method, which uses a function inside the rectangle function, to create each handle. Similar to the rectangles array. However I think this method will being becoming more and more complex.

--- rectangle.js
+++ rectangle.js
@@ -40,13 +40,11 @@ function app_init(initialValues){
                                                 curr.height,
                                                 curr.radius);
     }
-
-
-    this.getRectById = function(Identifier){ //return the object of the rectangle
-        return window.currentState[Identifier];
-    }
-
 } //end of app
+
+app_init.getRectById = function(Identifier){ //return the object of the rectangle
+    return window.currentState[Identifier];
+}
 
 function rectangle(id, x = 0, y = 0, width = 100, height = 100, radius = 0){
     this.id = id;
@@ -55,7 +53,7 @@ function rectangle(id, x = 0, y = 0, width = 100, height = 100, radius = 0){
     this.width = width;
     this.height = height;
     this.radius = radius;
-    this.element;
+    this.element; this.handles;
 
     this.draw = function(){
         var app = document.querySelector('#app');
@@ -71,20 +69,48 @@ function rectangle(id, x = 0, y = 0, width = 100, height = 100, radius = 0){
         rect.style.borderRadius = `${this.radius}px`;
         app.appendChild(rect);
         return rect;
+
     }
     this.element = this.draw();
 
     this.redraw = function(){
         var rect = document.querySelector(`[rect-data="${this.id}"]`); //get the edited element
-        rect.style.width = window.currentState[this.id].width + "px";
-        rect.style.height = window.currentState[this.id].height + "px";
-        rect.style.left = window.currentState[this.id].x + "px";
-        rect.style.top = window.currentState[this.id].y + "px";
-        rect.style.borderRadius = window.currentState[this.id].radius + "px";
+        rect.style.width = this.width + "px";
+        rect.style.height = this.height + "px";
+        rect.style.left = this.x + "px";
+        rect.style.top = this.y + "px";
+        rect.style.borderRadius = this.radius + "px";
         return true;
     }
 
-    
+    this.defaultHandles = [
+        {
+            id: 0,
+            name: 'top-resize'
+        }
+    ]
+
+    this.generateHandles = function(handles){
+        for(i=0; i<handles.length; i++){
+            var curr = handles[i];
+
+            return handles.currentState[i] = new handle( type,
+                                                        curr.x,
+                                                        curr.y,
+                                                        curr.width,
+                                                        curr.height,
+                                                        curr.radius);
+        }
+    }
+
+    this.handles = this.generateHandles(defaultHandles);
+
+    function handle(type){
+        switch(){
+            
+    }
+
+
     this.toJSON = function(){
         return { id: this.id, width: this.width, height: this.height, x: this.x, y: this.y, radius: this.radius };
     }
@@ -96,12 +122,12 @@ function rectangle(id, x = 0, y = 0, width = 100, height = 100, radius = 0){
     }
 
     this.getPosition = function(){
-        return {x: window.currentState[this.id].x, y: window.currentState[this.id].y};
+        return {x: this.x, y: this.y};
     }
 
     this.setPosition = function(x, y){
-        window.currentState[this.id].x = x;
-        window.currentState[this.id].y = y;
+        this.x = x;
+        this.y = y;
         this.redraw();
     }
 
@@ -154,13 +180,3 @@ function rectangle(id, x = 0, y = 0, width = 100, height = 100, radius = 0){
     document.addEventListener('mousemove', this.dragMove.bind(this))
 
 } //end of rectangle
-
-
-
-/*
-var rect = application.getRectById(1); 
-rect.setSize(100, 100); 
-rect.setPosition(10, 10); 
-rect.setCornerRadius(5); 
-expect(rect.toJSON()).eql({ id: 1, width: 100, height: 100, x: 10, y: 10, radius: 5 });
-*/
